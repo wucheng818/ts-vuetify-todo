265c96db8d4aadc62fa85cdcb8b032cdd944fc4d
orangeWU818wucheng <orangewu818@163.com>
2018-12-04 20:27:47 +0800

    第一版本

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Todo/>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Todo from './components/Todo.vue';
+
+@Component({
+  components: {
+    Todo,
+  },
+})
+export default class App extends Vue {}
+</script>
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
@@ -14,12 +21,5 @@
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+  overflow hidden
 </style>
--- src/store.ts
+++ src/store.ts
@@ -2,15 +2,64 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+interface initEdit {
+  title: string;
+  isChecked: boolean;
+}
+interface initState {
+  dataList: initEdit[] | [];
+}
+// localStorage.removeItem('todoList')
+const storage = {
+  write(key: string, value: any) {
+    if (value) {
+      value = JSON.stringify(value);
+    }
+    localStorage.setItem(key, value);
+  },
+  read<initEdit>(key: string): initEdit | [] {
+    const value: string | null = localStorage.getItem(key);
+    if (value && value != 'undefined' && value != 'null') {
+        return JSON.parse(value) as initEdit;
+    }
+    return [];
+  },
+};
 
+
+const state: initState = {
+  dataList: storage.read('todoList'),
+};
+const actions = {
+  addData(context: any, data: string) {
+    context.commit('addData', data);
+  },
+  editData(context: any, data: object[]) {
+    context.commit('editData', data);
+  },
+  deleteData(context: any, data: object[]) {
+    context.commit('deleteData', data);
+  },
+};
+const mutations = {
+  addData(state: any, data: string) {
+    state.dataList.push({
+        title: data,
+        isChecked: false,
+    });
+    storage.write('todoList', state.dataList);
+  },
+  editData(state: any, data: initEdit[]) {
+    state.dataList = data;
+    storage.write('todoList', data);
+  },
+  deleteData(state: any, data: initEdit[]) {
+    state.dataList = data;
+    storage.write('todoList', data);
+  },
+};
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state,
+  mutations,
+  actions,
 });
