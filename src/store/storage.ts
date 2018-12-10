3468ffe95830acffadf273990187d2ac0a7da015
orangeWU818wucheng <orangewu818@163.com>
2018-12-10 15:57:43 +0800

    fix:修改方法类型定义

--- src/store/storage.ts
+++ src/store/storage.ts
@@ -1,15 +1,15 @@
 import { initEdit } from '../components/type';
 export default {
-  write(key: string, value: any) {
+  write(key: string, value: any): void {
     if (value) {
       value = JSON.stringify(value);
     }
     localStorage.setItem(key, value);
   },
-  read<initEdit>(key: string): initEdit | [] {
+  read<initEdit>(key: string): initEdit[] | [] {
     const value: string | null = localStorage.getItem(key);
     if (value && value != 'undefined' && value != 'null') {
-      return JSON.parse(value) as initEdit;
+      return JSON.parse(value) as initEdit[];
     }
     return [];
   },
